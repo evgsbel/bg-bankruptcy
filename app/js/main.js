e522290524178aaf1fef03ae0979685c1f34a3f7
evgsbel <beloff163@gmail.com>
2019-10-22 17:16:56 +0400

    fixes

--- app/js/main.js
+++ app/js/main.js
@@ -2,7 +2,6 @@ let arrow_left = "<svg width='16' height='15' viewBox='0 0 16 15' fill='none' xm
     arrow_right = "<svg width='16' height='15' viewBox='0 0 16 15' fill='none' xmlns='http://www.w3.org/2000/svg'><path d='M0.999999 7.40234L14.5 7.40234M14.5 7.40234L8.5 13.4023M14.5 7.40234L8.5 1.40234'  stroke-width='2' stroke-linecap='round' stroke-linejoin='round'/></svg>";
 
 $('.video__slider-js').owlCarousel({
-    loop:true,
     dots: false,
     nav:true,
     navText: [arrow_left, arrow_right],
@@ -11,7 +10,8 @@ $('.video__slider-js').owlCarousel({
     margin: 30,
     responsive:{
         0:{
-            items:1
+            items:1,
+            margin: 0
         },
         600:{
             items:2
